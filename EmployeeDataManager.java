aac177daf4cd97cd4512dce792d7bce16713a1cb
Rabin Gaire <rabingaire20@gmail.com>
2017-01-22 07:51:44 +0545

    added select option

--- EmployeeDataManager.java
+++ EmployeeDataManager.java
@@ -1,18 +1,7 @@
 import java.util.*;
 public class EmployeeDataManager {
 
-  public void displayMenu() {
-    System.out.println("1. Input and validate data");
-    System.out.println("2. Display");
-    System.out.println("3. Sort by name");
-    System.out.println("4. Sort by hours");
-    System.out.println("5. Search by name");
-    System.out.println("6. Search by hours");
-    System.out.println("7. Statistics");
-    System.out.println("8. Exit");
-  }
-
-  public void displayTitle(String displayTitle, String displayDecoration) {
+  public EmployeeDataManager(String displayTitle, String displayDecoration) { //constructor
     System.out.println();
     for(int i = 0; i < 50; i++) {
       if(i < 29) {
@@ -32,13 +21,54 @@ public class EmployeeDataManager {
     System.out.println();
   }
 
+  public void displayMenu() {
+    System.out.println("1. Input and validate data");
+    System.out.println("2. Display");
+    System.out.println("3. Sort by name");
+    System.out.println("4. Sort by hours");
+    System.out.println("5. Search by name");
+    System.out.println("6. Search by hours");
+    System.out.println("7. Statistics");
+    System.out.println("8. Exit");
+  }
+
+  public void inputData() {
+    System.out.print("Input Data");
+  }
+
+  public void selectOption() {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter options 1-8: ");
+    int selectionValue = input.nextInt();
+    switch(selectionValue) {
+      case 1:
+        inputData();
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+      case 6:
+        break;
+      case 7:
+        break;
+      case 8:
+        break;
+      default:
+        System.out.println("Wrong Input");
+    }
+  }
+
   public static void main(String[] args) {
     //object for calling non-static methods
-    EmployeeDataManager dataManager = new EmployeeDataManager();
-    //displays the title of the program
-    dataManager.displayTitle("Employee Data Manager","-");
+    EmployeeDataManager dataManager = new EmployeeDataManager("Employee Data Manager","-");
     //displays the menu of the program
     dataManager.displayMenu();
-    
+    //calls the selectOption input module
+    dataManager.selectOption();
   }
 }
