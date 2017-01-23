acd28df0703da8e5a1c06e0213d04f1243abab6a
Rabin Gaire <rabingaire20@gmail.com>
2017-01-23 23:48:58 +0545

    Display system added

--- EmployeeDataManager.java
+++ EmployeeDataManager.java
@@ -1,26 +1,15 @@
 import java.util.*;
 public class EmployeeDataManager {
 
-  public String[] employeeNames;
-  public int[] workedHour;
+  public ArrayList<String> employeeNames = new ArrayList<String>();
+  public ArrayList<Integer> workedHours = new ArrayList<Integer>();
+  public ArrayList<Integer> wage = new ArrayList<Integer>();
 
-  public EmployeeDataManager(String displayTitle, String displayDecoration) { //constructor
+  public EmployeeDataManager() { //constructor
     System.out.println();
-    for(int i = 0; i < 50; i++) {
-      if(i < 29) {
-        System.out.print(" ");
-      } else if(i == 29) {
-        System.out.println(displayTitle);
-        System.out.println();
-      } else if(i>=30 && i<49) {
-        System.out.print(" ");
-      } else {
-        for(int j = 0; j < 45; j++) {
-          System.out.print(displayDecoration);
-        }
-        System.out.println();
-      }
-    }
+    System.out.println("\t\t\t\tEmployee Data Manager" );
+    System.out.println();
+    System.out.println("\t\t\t----------------------------------------");
     System.out.println();
   }
 
@@ -38,13 +27,25 @@ public class EmployeeDataManager {
   public void inputData() {
     Scanner input = new Scanner(System.in);
     System.out.print("Please enter employee name: ");
-    String employeeName = input.nextLine();
+    String name = input.nextLine();
     System.out.print("Please enter skill level (1, 2, 3): ");
     int skillLevel = input.nextInt();
-  }
+    System.out.print("Please enter worked hour: ");
+    int workedHour = input.nextInt();
 
-  public boolean isValidName(String name){
-    
+    if( workedHour >= 0 && workedHour <= 60) {
+      if (calculateWage(skillLevel, workedHour)) { //calling function to calculate wage
+        //adding data to ArrayList
+        employeeNames.add(name);
+        workedHours.add(workedHour);
+      }
+    } else  {
+      System.out.println("Worked hour must be at the range between 0 and 60 try agin!");
+    }
+
+    System.out.println(" ");
+    displayMenu();
+    selectOption();
   }
 
   public void selectOption() {
@@ -56,6 +57,7 @@ public class EmployeeDataManager {
         inputData();
         break;
       case 2:
+        display();
         break;
       case 3:
         break;
@@ -68,15 +70,54 @@ public class EmployeeDataManager {
       case 7:
         break;
       case 8:
+        System.out.println("Thank You");
         break;
       default:
         System.out.println("Wrong Input");
     }
   }
 
+  public void display() {
+    System.out.println("\t\t\t\tEmployee worked hours & wage");
+    System.out.println("\t\t\t========================================");
+    System.out.println("\t\t\tEmployee\tWorked hours\tWage");
+    System.out.println("\t\t\t----------------------------------------");
+
+    for (int i = 0; i < employeeNames.size(); i++ ) {
+      System.out.println("\t\t\t" + employeeNames.get(i) + "\t" + workedHours.get(i) + "\t\t$" + wage.get(i) );
+    }
+
+    System.out.println();
+    System.out.println("\t\t\tTotal:" + employeeNames.size() + " data entries");
+
+    System.out.println();
+    displayMenu();
+    selectOption();
+  }
+
+  public boolean calculateWage(int skillLevel, int workedHour){
+    switch(skillLevel) {
+      case 1:
+        int wageSkillOne = workedHour * 15;
+        wage.add(wageSkillOne);
+        return true;
+      case 2:
+        int wageSkillTwo = workedHour * 17;
+        wage.add(wageSkillTwo);
+        return true;
+      case 3:
+        int wageSkillThree = workedHour * 21;
+        wage.add(wageSkillThree);
+        return true;
+      default:
+        System.out.println("Wrong Input skillLevel cannot be " + skillLevel + " try again!");
+        return false;
+    }
+  }
+
   public static void main(String[] args) {
     //object for calling non-static methods
-    EmployeeDataManager dataManager = new EmployeeDataManager("Employee Data Manager","-");
+    EmployeeDataManager dataManager = new EmployeeDataManager();
     //displays the menu of the program
     dataManager.displayMenu();
     //calls the selectOption input module
